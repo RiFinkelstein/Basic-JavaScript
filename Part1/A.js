450e017ab50f1ddbce74a538770c74f6e14e1130
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-08 16:39:37 +0200

    Update A.js

--- Part1/A.js
+++ Part1/A.js
@@ -1,1 +1,5 @@
 const myArray = [["RIVKA", 20], ["HENNA", 24]]; //nest one array into another array
+
+const myArray = [50, 60, 70];
+const myData= myArray[0]
+console.log(myData); //choosing 1st item in array to print
