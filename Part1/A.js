5b0d851fab403c63f1ed18a74f38b7f367ca45eb
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-08 16:40:59 +0200

    Update A.js

--- Part1/A.js
+++ Part1/A.js
@@ -1,5 +1,10 @@
 const myArray = [["RIVKA", 20], ["HENNA", 24]]; //nest one array into another array
 
+//choosing 1st item in array to print
 const myArray = [50, 60, 70];
 const myData= myArray[0]
-console.log(myData); //choosing 1st item in array to print
+console.log(myData); 
+
+//modify array with index
+const myArray = [18, 64, 99];
+myArray[0]=45; 
