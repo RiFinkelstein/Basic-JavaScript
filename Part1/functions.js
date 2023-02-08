8d799a042d99b390796d21aacbd8372e8f2df2ab
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-08 16:53:23 +0200

    Update functions.js

--- Part1/functions.js
+++ Part1/functions.js
@@ -10,3 +10,11 @@ function functionWithArgs(x ,y){
   console.log(x/ y)
 }
 functionWithArgs(12 ,2)
+
+//using return in functions
+function timesFive(num) {
+  return num * 5;
+}
+timesFive(5);
+timesFive(2);
+timesFive(0);
