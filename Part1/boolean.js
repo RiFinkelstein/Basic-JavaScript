56eea05678936c6bc053555c7800a1d2d02b0d39
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-18 23:59:01 +0200

    Update boolean.js

--- Part1/boolean.js
+++ Part1/boolean.js
@@ -19,3 +19,13 @@ function testEqual(val) {
   return "Not Equal";
 }
 testEqual(10);
+
+//===
+function testStrict(val) {
+  if (val===7) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
