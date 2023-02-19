f447cf224adb01351fcb743b307c32a9b4121cad
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-19 19:14:11 +0200

    Update operators.js

--- Part1/operators.js
+++ Part1/operators.js
@@ -66,3 +66,18 @@ function testGreaterThan(val) {
 }
 
 testGreaterThan(10);
+
+//>=
+function testGreaterOrEqual(val) {
+  if (val>=20) {  
+    return "20 or Over";
+  }
+
+  if (val>=10) { 
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
