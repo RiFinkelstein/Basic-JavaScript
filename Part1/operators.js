199fe99f06b49eece1e17661741306c61d151382
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-22 19:54:28 +0200

    Update operators.js

--- Part1/operators.js
+++ Part1/operators.js
@@ -181,15 +181,17 @@ orderMyLogic(7);
 //elseif/else/if
 function testSize(num) {
   if (num < 5) {
-  return "Tiny"}
-  else if (num < 10) {
-  return "Small"
-} else if (num<15) {
-  return "Medium"}
- else if(num<20){
-   return "Large"
- }
-else {
-  return "Huge"
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+}
 
 testSize(7);
+
