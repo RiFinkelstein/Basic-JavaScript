5f696d3719b29ce4de23919e4174e15aced4368d
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-27 12:26:08 +0200

    Update swtich.js

--- Part1/swtich.js
+++ Part1/swtich.js
@@ -115,3 +115,33 @@ function abTest(a, b) {
 }
 
 abTest(2,2);
+
+//card game
+let count = 0;
+
+function cc(card) {
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'K':
+    case 'J':
+    case 'Q':
+    case 'A':
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  return "Change Me";
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
