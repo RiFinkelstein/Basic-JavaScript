e6b9808aba026b94d8b70446cc1a7d7ed96b3b7a
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-27 12:06:35 +0200

    Update swtich.js

--- Part1/swtich.js
+++ Part1/swtich.js
@@ -47,3 +47,30 @@ function switchOfStuff(val) {
 }
 
 switchOfStuff(1);
+
+//multiple identical options
+function sequentialSizes(val) {
+  let answer = "";
+  switch(val){
+  case 1:
+  case 2:
+  case 3:
+    answer="low"
+    break
+  case 4:
+  case 5:
+  case 6:
+    answer="mid"
+    break
+  case 7:
+  case 8:
+  case 9:
+    answer="high"
+    break
+
+
+  }
+  return answer;
+}
+
+sequentialSizes(1);
