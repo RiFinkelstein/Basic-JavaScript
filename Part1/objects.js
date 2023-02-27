78feb23e99c2f1340c72ec1a5e8428359f3ad994
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-27 12:50:59 +0200

    Update objects.js

--- Part1/objects.js
+++ Part1/objects.js
@@ -26,13 +26,11 @@ const entreeValue = testObj["an entree"];
 const drinkValue = testObj["the drink"];   
 
 //accessing object properties using variables
-// Setup
 const testObj = {
   12: "Namath",
   16: "Montana",
   19: "Unitas"
 };
 
-// Only change code below this line
-const playerNumber = 16;  // Change this line
-const player = testObj[playerNumber];   // Change this line
+const playerNumber = 16;  
+const player = testObj[playerNumber];  
