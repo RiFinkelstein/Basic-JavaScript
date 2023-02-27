82ce329863befd20c3431fb017af0be3aa960ebe
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-02-27 12:09:37 +0200

    Update swtich.js

--- Part1/swtich.js
+++ Part1/swtich.js
@@ -74,3 +74,28 @@ function sequentialSizes(val) {
 }
 
 sequentialSizes(1);
+
+//from if statment to swithc statement
+function chainToSwitch(val) {
+  let answer = "";
+  switch(val){
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break
+  case 7:
+    answer = "Ate Nine";
+    break
+  }
+  return answer;
+}
+
+chainToSwitch(7);
