48a2ae3668d2ab62f02cc6c77ea4b7b79d2d4e4a
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-01 12:00:40 +0200

    Update loops.js

--- Part1/loops.js
+++ Part1/loops.js
@@ -26,3 +26,10 @@ const myArray = [];
 for (let i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
+
+//iterating through for loops 
+const myArr = [2, 3, 4, 5, 6];
+let total=0
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
