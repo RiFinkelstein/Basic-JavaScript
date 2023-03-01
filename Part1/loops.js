7c51abe414f7328a3776e19ef5c4758eb6892067
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-01 11:49:49 +0200

    Update loops.js

--- Part1/loops.js
+++ Part1/loops.js
@@ -13,3 +13,12 @@ const myArray = [];
 for (let i=1; i<=5; i++){
   myArray.push(i);
 }
+
+//odd numbwer for loop
+// Setup
+const myArray = [];
+  
+// Only change code below this line
+for (let i = 1; i < 11; i += 2) {
+  myArray.push(i);
+}
