9eea2614abee95e230067359a6c6865b17cf1bce
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-01 12:16:42 +0200

    Update loops.js

--- Part1/loops.js
+++ Part1/loops.js
@@ -33,3 +33,16 @@ let total=0
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
+
+
+//nesting for loops
+function multiplyAll(arr) {
+  let product = 1;
+=for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    product= product*arr[i][j];
+  }
+}  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
