964c002e47ec3a01dd0cb454965c9740666d8442
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-03 12:16:34 +0200

    Create Recursion.js

---
+++ Recursion.js
@@ -0,0 +1,23 @@
+//Use Recursion to Create a Countdown
+function countdown(n){
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
+}
+
+//Use Recursion to Create a Range of Numbers
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+
+};
+
