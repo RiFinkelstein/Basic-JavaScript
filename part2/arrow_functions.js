d0f4c3b9559a157d9dd5018f0d24c1b634a2a476
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-03-05 11:50:26 +0200

    Update arrow_functions.js

--- part2/arrow_functions.js
+++ part2/arrow_functions.js
@@ -2,3 +2,8 @@
 const magic = () => {
   return new Date();
 };
+
+//Write Arrow Functions with Parameters
+const myConcat = (arr1, arr2)=>  arr1.concat(arr2);
+
+console.log(myConcat([1, 2], [3, 4, 5]));
