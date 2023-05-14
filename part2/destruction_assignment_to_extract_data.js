8f404543ad139c26797c547d35ef78b6d8eb147d
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-05-14 11:58:45 -0700

    Create destruction_assignment_to_extract_data.js

---
+++ part2/destruction_assignment_to_extract_data.js
@@ -0,0 +1,8 @@
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+//this is the smae as writing HIGH_TEMPERATURES.today and HIGH_TEMPERATURES.tomorrow
+const {today, tomorrow} = HIGH_TEMPERATURES;
