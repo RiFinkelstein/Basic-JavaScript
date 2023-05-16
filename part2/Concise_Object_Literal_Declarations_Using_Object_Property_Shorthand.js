368ae9f9391fa15a496d113958b67ed014ddf2d4
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-05-16 09:33:49 -0700

    Create Concise_Object_Literal_Declarations_Using_Object_Property_Shorthand.js

---
+++ part2/Concise_Object_Literal_Declarations_Using_Object_Property_Shorthand.js
@@ -0,0 +1,12 @@
+//const createPerson = (name, age, gender) => {
+ // return {
+   // name: name,
+    //age: age,
+    //gender: gender
+  //};
+  // Only change code above this line
+//};
+//above is same as below
+const createPerson = (name, age, gender) => ({
+  name, age, gender
+});
