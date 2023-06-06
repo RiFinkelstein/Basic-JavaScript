7b82e0b71a3d963ffc8aa5e386d54a0d04e9ccd1
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-06-06 09:55:09 -0700

    Create console.clear.js

---
+++ part 4: debugging/console.clear.js
@@ -0,0 +1,4 @@
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output)
+//use console.clear to clear from browser console
+console.clear(output)
