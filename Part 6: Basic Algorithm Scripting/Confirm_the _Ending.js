02d59d8b803ddaaae8f323dc96ceb700847b82db
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-06-25 19:52:10 +0300

    Delete Confirm_the _Ending.js

--- Part 6: Basic Algorithm Scripting/Confirm_the _Ending.js
+++
@@ -1,8 +0,0 @@
-function confirmEnding(str, target) {
-  // "Never give up and good luck will find you."
-  // -- Falcor
-
-  return str.slice(str.length - target.length) === target;
-}
-
-confirmEnding("He has to give me a new name", "name");
