c52bf4cd460548badbf6c63b7174a389569f3ebe
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2023-07-01 13:43:15 -0700

    Create Use_a_Constructor_to_Create_Objects.js

---
+++ part 7:Object Oriented Programming/Use_a_Constructor_to_Create_Objects.js
@@ -0,0 +1,7 @@
+function Dog() {
+  this.name = "chocolate";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+//se the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound
+let hound= new Dog()
